0e785a077bd9f90c30a2ed60361b747aa5b30f6e
t-iwano <t-iwano@axsh.net>
2017-07-25 18:27:38 +0900

    Changed build script

--- ci/citest/build.sh
+++ ci/citest/build.sh
@@ -1,1 +1,25 @@
-echo "start sandbox build"
+#!/bin/bash
+# Run build process
+
+set -ex -o pipefail
+SCRIPT_DIR="$(cd "$(dirname $(readlink -f "$0"))" && pwd -P)"
+
+# check the step file.
+STEP_LIST_PATH=${SCRIPT_DIR}/steplist.conf
+if [[ -n "${STEP_LIST_PATH}" && ! -f "${STEP_LIST_PATH}" ]]; then
+  echo "ERROR": Can't find the file: ${STEP_LIST_PATH}" >&2
+  exit 1
+fi
+
+for step in $(cat ${STEP_LIST_PATH}); do
+  STEP_DIR=${SCRIPT_DIR}/${step}
+  STEP_SCRIPT_PATH=${STEP_DIR}/build.sh
+
+  # check the build script file.
+  if [[ -n "${STEP_SCRIPT_PATH}" && ! -f "${STEP_SCRIPT_PATH}" ]]; then
+    echo "ERROR": Can't find the file: ${STEP_SCRIPT_PATH}" >&2
+    exit 1
+  fi
+  /bin/bash -c "${STEP_SCRIPT_PATH}"
+done
+
