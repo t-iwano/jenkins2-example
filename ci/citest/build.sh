14eff9a33d6d34d1ab0f2a4438dd2e064273874d
t-iwano <t-iwano@axsh.net>
2017-07-25 18:39:30 +0900

    Fixed syntax messages.

--- ci/citest/build.sh
+++ ci/citest/build.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR="$(cd "$(dirname $(readlink -f "$0"))" && pwd -P)"
 # check the step file.
 STEP_LIST_PATH=${SCRIPT_DIR}/steplist.conf
 if [[ -n "${STEP_LIST_PATH}" && ! -f "${STEP_LIST_PATH}" ]]; then
-  echo "ERROR": Can't find the file: ${STEP_LIST_PATH}" >&2
+  echo "ERROR: Can't find the file: ${STEP_LIST_PATH}" >&2
   exit 1
 fi
 
@@ -15,9 +15,10 @@ for step in $(cat ${STEP_LIST_PATH}); do
   STEP_DIR=${SCRIPT_DIR}/${step}
   STEP_SCRIPT_PATH=${STEP_DIR}/build.sh
 
+  echo "${STEP_SCRIPT_PATH}"
   # check the build script file.
   if [[ -n "${STEP_SCRIPT_PATH}" && ! -f "${STEP_SCRIPT_PATH}" ]]; then
-    echo "ERROR": Can't find the file: ${STEP_SCRIPT_PATH}" >&2
+    echo "ERROR: Can't find the file: ${STEP_SCRIPT_PATH}" >&2
     exit 1
   fi
   /bin/bash -c "${STEP_SCRIPT_PATH}"
